028246981377064bc4a6e5e58c9e434eb766691c
Immanuella Umoren <immanuellaumoren@gmail.com>
2024-04-02 21:43:19 -0500

    Updates

--- DormDeals/ViewController.swift
+++ DormDeals/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func button1Tapped(_ sender: UIButton) {
+        
+        print("Button 1 was tapped.")
+        storeUserSelection(selection: "shopper")
+        
+    }
+    
+    @IBAction func button2Tapped(_ sender: UIButton) {
+       
+        print("Button 2 was tapped.")
+        storeUserSelection(selection: "businessOwner")
+    }
+    
+    @IBAction func button3Tapped(_ sender: UIButton) {
+        print("Button 3 was tapped.")
+        storeUserSelection(selection: "shortTimeSale")
+        
+    }
+    
+    func storeUserSelection(selection: String) {
+        UserDefaults.standard.set(selection, forKey: "userType")
+        print(selection)
+    }
+    
+    
+    
+    
 }
 
